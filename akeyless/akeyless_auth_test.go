e9fd984edc15b891b50be245546f33a19419247f
Your Name <you@example.com>
2023-10-24 09:00:00 -0400

    aider: Added test cases for when the command is a valid command and when it is an invalid command.
    # Context:
    USER: now lets do what you suggested and create a test for when the command is a valid command and one where it is an invalid command
    ASSISTANT: Great, let's add those test cases to the `akeyless/akeyless_auth_test.go` file.
    
    Here are the changes:
    
    ```go
    akeyless/akeyless_auth_test.go
    <<<<<<< ORIGINAL
    package akeyless
    
    import (
    	"os"
    	"testing"
    )
    
    func TestGetTokenFromAkeylessCommandLine(t *testing.T) {
    	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is not set
    	os.Unsetenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND")
    	_, err := GetTokenFromAkeylessCommandLine()
    	if err == nil {
    		t.Errorf("Expected error when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is not set, got nil")
    	}
    }
    =======
    package akeyless
    
    import (
    	"os"
    	"testing"
    )
    
    func TestGetTokenFromAkeylessCommandLine(t *testing.T) {
    	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is not set
    	os.Unsetenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND")
    	_, err := GetTokenFromAkeylessCommandLine()
    	if err == nil {
    		t.Errorf("Expected error when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is not set, got nil")
    	}
    
    	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to a valid command
    	os.Setenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND", "echo test")
    	token, err := GetTokenFromAkeylessCommandLine()
    	if err != nil || token != "test" {
    		t.Errorf("Expected token 'test', got '%s', error: %v", token, err)
    	}
    
    	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to an invalid command
    	os.Setenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND", "invalid_command")
    	_, err = GetTokenFromAkeylessCommandLine()
    	if err == nil {
    		t.Errorf("Expected error when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to an invalid command, got nil")
    	}
    }
    >>>>>>> UPDATED
    ```
    
    In the second test case, we set `AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND` to a valid command (`echo test`) and check if the function returns the correct token.
    
    In the third test case, we set `AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND` to an invalid command and check if the function returns an error.

--- akeyless/akeyless_auth_test.go
+++ akeyless/akeyless_auth_test.go
@@ -12,4 +12,18 @@ func TestGetTokenFromAkeylessCommandLine(t *testing.T) {
 	if err == nil {
 		t.Errorf("Expected error when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is not set, got nil")
 	}
+
+	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to a valid command
+	os.Setenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND", "echo test")
+	token, err := GetTokenFromAkeylessCommandLine()
+	if err != nil || token != "test" {
+		t.Errorf("Expected token 'test', got '%s', error: %v", token, err)
+	}
+
+	// Test case when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to an invalid command
+	os.Setenv("AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND", "invalid_command")
+	_, err = GetTokenFromAkeylessCommandLine()
+	if err == nil {
+		t.Errorf("Expected error when AKEYLESS_CLI_AUTHENTICATION_TOKEN_COMMAND is set to an invalid command, got nil")
+	}
 }
