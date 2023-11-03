b878849a00a10e5d1521e5a1e219a96e776ddcc2
Your Name <you@example.com>
2023-11-03 11:45:09 -0400

    aider: Test the NewConfig function by creating a Config struct with specific values for each field and checking that each field is set correctly.
    # Aider chat conversation:
    
    USER: ok, start by filling out the TestNewConfig function based on the source function
    
    ASSISTANT: Sure, let's start by writing a test for the `NewConfig` function. This function takes five parameters and returns a `Config` struct. The test should create a `Config` struct using `NewConfig` and then check that the fields of the struct are set correctly.
    
    Here's how you can do it:
    
    ```go
    sheller/config_test.go
    <<<<<<< SEARCH
    func TestNewConfig(t *testing.T) {
    	// Test cases to be added
    }
    =======
    func TestNewConfig(t *testing.T) {
    	cliPath := "/path/to/cli"
    	profile := "testProfile"
    	akeylessPath := "/path/to/akeyless"
    	expiryBuffer := 10 * time.Minute
    	debug := true
    
    	config := NewConfig(cliPath, profile, akeylessPath, expiryBuffer, debug)
    
    	if config.CLIPath != cliPath {
    		t.Errorf("Expected CLIPath to be %s, but got %s", cliPath, config.CLIPath)
    	}
    	if config.Profile != profile {
    		t.Errorf("Expected Profile to be %s, but got %s", profile, config.Profile)
    	}
    	if config.AkeylessPath != akeylessPath {
    		t.Errorf("Expected AkeylessPath to be %s, but got %s", akeylessPath, config.AkeylessPath)
    	}
    	if config.ExpiryBuffer != expiryBuffer {
    		t.Errorf("Expected ExpiryBuffer to be %s, but got %s", expiryBuffer, config.ExpiryBuffer)
    	}
    	if config.Debug != debug {
    		t.Errorf("Expected Debug to be %v, but got %v", debug, config.Debug)
    	}
    }
    >>>>>>> REPLACE
    ```
    
    This test creates a new `Config` struct with specific values for each field, and then checks that each field of the struct is set to the expected value. If any field is not set correctly, the test fails with an error message.

--- sheller/config_test.go
+++ sheller/config_test.go
@@ -6,7 +6,29 @@ import (
 )
 
 func TestNewConfig(t *testing.T) {
-	// Test cases to be added
+	cliPath := "/path/to/cli"
+	profile := "testProfile"
+	akeylessPath := "/path/to/akeyless"
+	expiryBuffer := 10 * time.Minute
+	debug := true
+
+	config := NewConfig(cliPath, profile, akeylessPath, expiryBuffer, debug)
+
+	if config.CLIPath != cliPath {
+		t.Errorf("Expected CLIPath to be %s, but got %s", cliPath, config.CLIPath)
+	}
+	if config.Profile != profile {
+		t.Errorf("Expected Profile to be %s, but got %s", profile, config.Profile)
+	}
+	if config.AkeylessPath != akeylessPath {
+		t.Errorf("Expected AkeylessPath to be %s, but got %s", akeylessPath, config.AkeylessPath)
+	}
+	if config.ExpiryBuffer != expiryBuffer {
+		t.Errorf("Expected ExpiryBuffer to be %s, but got %s", expiryBuffer, config.ExpiryBuffer)
+	}
+	if config.Debug != debug {
+		t.Errorf("Expected Debug to be %v, but got %v", debug, config.Debug)
+	}
 }
 
 func TestNewConfigWithDefaults(t *testing.T) {
